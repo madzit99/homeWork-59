f984c25f9200de711c46830560db6ac9cc331b88
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-13 21:04:10 +0600

    создал функцию редактирования и удаления, прокинул в карточки

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Form from "./Components/Form/Form";
 import { Movie } from "./types";
 import MovieCard from "./Components/MovieCard/MovieCard";
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
-  
+
   const addMovie = (name: string) => {
     const newMovie: Movie = {
       id: Math.random(),
@@ -14,13 +14,29 @@ const App = () => {
     setMovies((prevMovies) => [...prevMovies, newMovie]);
   };
 
+  const editMovie = useCallback((id: number, name: string) => {
+    setMovies((prevMovies) =>
+      prevMovies.map((movie) =>
+        movie.id === id ? { ...movie, name: name } : movie
+      )
+    );
+  }, []);
+
+  const deleteMovie = useCallback((id: number) => {
+    setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+  }, []);
+
   return (
     <>
       <div className="container w-50">
         <div className="task1">
-          <Form onAdd={addMovie}/>
+          <Form onAdd={addMovie} />
           {movies.map((movie) => (
-            <MovieCard movie={movie}/>
+            <MovieCard
+              movie={movie}
+              onEdit={editMovie}
+              onDelete={deleteMovie}
+            />
           ))}
         </div>
         <div className="task2"></div>
--- src/Components/MovieCard/MovieCard.tsx
+++ src/Components/MovieCard/MovieCard.tsx
@@ -3,14 +3,31 @@ import { Movie } from "../../types";
 
 interface Props {
   movie: Movie;
+  onEdit: (id: number, name: string) => void;
+  onDelete: (id: number) => void;
 }
 
-const MovieCard: React.FC<Props> = ({ movie }) => {
+const MovieCard: React.FC<Props> = ({ movie, onEdit, onDelete }) => {
   const [movieName, setMovieName] = useState<string>(movie.name);
+
+  const handleEdit = () => {
+    onEdit(movie.id, movieName);
+  };
+
+  const handleDelete = () => {
+    onDelete(movie.id);
+  };
   return (
     <div className="input-container d-flex align-items-center gap-3 mb-3">
-      <input type="text" className="form-control" name="movieInput" value={movieName} />
-      <button className="btn btn-close"></button>
+      <input
+        type="text"
+        className="form-control"
+        name="movieInput"
+        value={movieName}
+        onChange={(e) => setMovieName(e.target.value)}
+        onBlur={handleEdit}
+      />
+      <button className="btn btn-close" onClick={handleDelete}></button>
     </div>
   );
 };
